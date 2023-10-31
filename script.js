604ca05226a83dc08b48b1976c0d6bae7807cef8
Alvin Ström <alvinstroom@gmail.com>
2023-10-31 11:58:27 +0200

    Added index and script

---
+++ script.js
@@ -0,0 +1,22 @@
+const namn = "JavaScript";
+const version = "1.0";
+
+
+const str1 =  `Hello ${namn} version ${version}!`;
+
+
+function hello() {
+    const str2 =  ` Hello function!`;
+    console.log (str2);
+}
+
+
+function btnHandler(){
+    console.log ("Du tryckte på knappen!")
+
+}
+
+hello();
+
+console.log (str1);
+
